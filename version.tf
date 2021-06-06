e8745185ca9b0cdf904791a0a0192421ac323a75
vinayak <vinayak@vinayaks-MacBook-Air.local>
2021-06-07 04:23:17 +0530

    Added terraform cloud config and workflow for terraform

---
+++ version.tf
@@ -0,0 +1,38 @@
+terraform {
+  required_version = ">= 0.13"
+
+  required_providers {
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 0.10.4"
+    }
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 3.62.0"
+    }
+    google-beta = {
+      source  = "hashicorp/google-beta"
+      version = "~> 3.64.0"
+    }
+    gsuite = {
+      source  = "deviavir/gsuite"
+      version = "~> 0.1.61"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.0.3"
+    }
+    random = {
+      source = "hashicorp/random"
+    }
+  }
+
+    backend "remote" {
+    organization = "sigmoid-devops"
+
+    workspaces {
+      name = "terraform-devops"
+    }
+  }
+
+}

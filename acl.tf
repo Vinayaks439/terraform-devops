9f54df9ec5e77655580a662a315f38588176ca0f
Vinayak S <vinayaks439@gmail.com>
2021-07-01 04:49:27 +0530

    Update acl.tf

--- acl.tf
+++ acl.tf
@@ -6,12 +6,12 @@ resource "google_project_iam_member" "billingadmin" {
 
   project = data.google_project.self.project_id
   role = "roles/billing.admin"
-  member = "vinayak@vernal-dispatch-281311.iam.gserviceaccount.com"
+  member = "serviceAccount:vinayak@vernal-dispatch-281311.iam.gserviceaccount.com"
 
 }
 
 resource "google_project_iam_member" "pubsubadmin" {
     project = data.google_project.project.project_id
     role = "roles/pubsub.admin"
-    member = "vinayak@vernal-dispatch-281311.iam.gserviceaccount.com"
+    member = "serviceAccount:vinayak@vernal-dispatch-281311.iam.gserviceaccount.com"
 }
